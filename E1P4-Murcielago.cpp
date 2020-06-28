4ace6dbbfb3e9f0f5d2e1ee4d3a2dfedaf470fb3
Nataly-Mangandi <00108520@uca.edu.sv>
2020-06-27 20:00:30 -0600

    PARCIAL4

--- E1P4-Murcielago.cpp
+++ E1P4-Murcielago.cpp
@@ -1,29 +1,46 @@
+// Example program
 #include <iostream>
-#include <string.h>
+#include <math.h>
 using namespace std;
 
-void codificar(string, int c); //string codificar(string)
+void codificarPalabra(char[]);
 
-int main(void){
-    string palabra;
-    int c=0;
-    printf("// Clave MURCIELAGO //");
-    printf("\nIngrese una palabra: ");
-    getline(cin, palabra);
-    c=palabra.length();//cout<<codificar(palabra)
-    printf("%d", c);
-    codificar(palabra, c);
+int main (void){
+    char palabra[30];
+    printf("Digite una palabra: ");
+    cin.getline(palabra, 30);
+    codificarPalabra(palabra);
+    printf("%s", palabra);
+    
 }
 
-void codificar(string palabra, int c){
+void codificarPalabra(char palabra[]){
     int i;
-    string np;
-    for (i=0; i<c; i++){
-        if (palabra[i]=='M' || palabra[i]=='m'){
-            np[i]='0';
-        } else {
-            np[i]=palabra[i];
+    for (i=0; i<30; i++){
+        switch(palabra[i]){
+            case 'M': palabra[i] = '0'; break;
+            case 'm': palabra[i] = '0'; break;
+            case 'U': palabra[i] = '1'; break;
+            case 'u': palabra[i] = '1'; break;
+            case 'R': palabra[i] = '2'; break;
+            case 'r': palabra[i] = '2'; break;
+            case 'C': palabra[i] = '3'; break;
+            case 'c': palabra[i] = '3'; break;
+            case 'I': palabra[i] = '4'; break;
+            case 'i': palabra[i] = '4'; break;
+            case 'E': palabra[i] = '5'; break;
+            case 'e': palabra[i] = '5'; break;
+            case 'L': palabra[i] = '6'; break;
+            case 'l': palabra[i] = '6'; break; 
+            case 'A': palabra[i] = '7'; break;
+            case 'a': palabra[i] = '7'; break; 
+            case 'G': palabra[i] = '8'; break;
+            case 'g': palabra[i] = '8'; break;
+            case 'O': palabra[i] = '9'; break;
+            case 'o': palabra[i] = '9'; break;
         }
     }
-    printf("%s", np);
+    
+    
 }
+
